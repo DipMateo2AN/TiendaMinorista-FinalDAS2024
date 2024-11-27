6bf964519faacc8d5af6beb43a90dc8c6c152ac7
Mateo Dip <Mateo.Dip@alumnos.uai.edu.ar>
2024-11-27 12:25:57 -0300

    clases casi terminadas, faltan metodos

---
+++ Modelo/DetalleFactura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class DetalleFactura
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Subtotal {  get; set; }
+        public Factura Factura { get; set; }
+
+    }
+}
---
+++ Modelo/Factura.cs
@@ -0,0 +1,33 @@
+﻿namespace Modelo
+{
+    public class Factura
+    {
+        public string Numero { get; set; }
+        public DateTime Fecha { get; set; }
+        public Cliente Cliente { get; set; }
+        public decimal Total { get; set; }
+
+        public List<DetalleFactura> DetallesFactura;
+
+        public Factura()
+        {
+            DetallesFactura = new List<DetalleFactura>();
+        }
+
+        public bool AgregarDetalleFactura(DetalleFactura nuevoDetalle)
+        {
+            var detalleRepetido = DetallesFactura.FirstOrDefault(d => d.Producto == nuevoDetalle.Producto);
+
+            detalleRepetido.Cantidad += nuevoDetalle.Cantidad;
+            return true;
+        }
+
+        public bool EliminarDetalleFactura(DetalleFactura detalleAEliminar)
+        {
+
+            return false;
+        }
+
+
+    }
+}
